9011ede9f355795b346206d4db7fc77c23de6f31
Shuhrat55 <149987706+Shuhrat55@users.noreply.github.com>
2026-04-28 05:45:22 +0300

    router.go

--- internal/delivery/gin/router.go
+++ internal/delivery/gin/router.go
@@ -1,45 +1,81 @@
 package gin
 
 import (
-	"github.com/gin-contrib/cors"
-	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	_ "github.com/Shuhrat55/auth/docs"
-	"github.com/Shuhrat55/auth/internal/delivery/gin/handlers"
-	"github.com/Shuhrat55/auth/internal/usecase"
+	"github.com/Shuhrat55/auth/pkg/client"
+	"github.com/Shuhrat55/forum/internal/delivery/gin/handler"
+	"github.com/Shuhrat55/forum/internal/usecase"
+	"github.com/Shuhrat55/forum/pkg/wsserver"
+
+	_ "github.com/Shuhrat55/forum/docs"
 	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter(userUseCase usecase.UseCase) *gin.Engine {
+func SetupRouter(P usecase.PostUseCase, T usecase.ThreadUseCase, authClient *client.AuthClient, hub *wsserver.Hub) *gin.Engine {
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}))
 
-	userHandler := handlers.NewUserHandler(userUseCase)
-	api := router.Group("/api/v1")
+	forumHandler := NewForumHandler(P, T)
+	go hub.Run()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	api := router.Group("/api/v2")
 	{
-		api.POST("/login", userHandler.Login)
-		api.POST("/refresh", userHandler.RefreshToken)
-		api.POST("/users", userHandler.Create)
-		api.GET("/users", userHandler.GetAll)
-		api.GET("/user/:id", userHandler.GetByID)
-		auth := api.Group("/")
-		auth.Use(handlers.AuthMiddleware())
+		api.GET("/threads", forumHandler.GetAllThread)
+		api.GET("/thread/:id", forumHandler.GetThreadByID)
+
+		authGroup := api.Group("")
+		authGroup.Use(handler.AuthMiddleware(authClient))
 		{
-			auth.GET("/users/:email", userHandler.GetByEmail)
-			auth.PUT("/users/:id", userHandler.UpdatePassword)
-			auth.DELETE("/users/:id", userHandler.Delete)
-			auth.POST("/user/:id", userHandler.CheckPassword)
-			auth.GET("/logout", userHandler.Logout)
+			authGroup.POST("/threads", forumHandler.CreateThread)
+			authGroup.POST("/threads/posts", forumHandler.CreatePost)
+
+			authGroup.GET("/threads/user/:id", forumHandler.GetThreadsByUserID)
+			authGroup.GET("/posts/user/:id", forumHandler.GetPostsByUserID)
+			authGroup.GET("/thread/:id/posts", forumHandler.GetPostsByThreadID)
+
+			authGroup.DELETE("/posts/:id", forumHandler.DeletePostByID)
+			authGroup.DELETE("/threads/:id", forumHandler.DeleteTheadByID)
+
+			authGroup.PUT("/threads", forumHandler.EditThread)
+
+			api.GET("/ws/threads/:id", hub.ThreadChat)
 		}
 	}
-	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	apiV1 := router.Group("/api/v1")
+	{
+		apiV1.GET("/threads", forumHandler.GetAllThread)
+		apiV1.GET("/thread/:id", forumHandler.GetThreadByID)
+
+	authGroupV1 := apiV1.Group("")
+	authGroupV1.Use(handler.AuthMiddleware(authClient))
+	{
+		authGroupV1.POST("/threads", forumHandler.CreateThread)
+		authGroupV1.POST("/threads/posts", forumHandler.CreatePost)
+
+		authGroupV1.GET("/threads/user/:id", forumHandler.GetThreadsByUserID)
+		authGroupV1.GET("/posts/user/:id", forumHandler.GetPostsByUserID)
+		authGroupV1.GET("/thread/:id/posts", forumHandler.GetPostsByThreadID)
+
+		authGroupV1.DELETE("/posts/:id", forumHandler.DeletePostByID)
+		authGroupV1.DELETE("/threads/:id", forumHandler.DeleteTheadByID)
+
+		authGroupV1.PUT("/threads", forumHandler.EditThread)
+
+		apiV1.GET("/ws/threads/:id", hub.ThreadChat)
+	}
+
 	return router
 }
